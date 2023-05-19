2fb0b3382e0a40e3c16bf79b46d4e751bc5f3b0e
cosBett <cosmasbett9@gmail.com>
2023-05-19 04:11:22 +0300

    generating 6-digit otp

--- otp.py
+++ otp.py
@@ -17,3 +17,17 @@ time_bytes = struct.pack('>Q', time_steps)
 # Calculate the HMAC-SHA1 hash of the time steps using the secret key
 hash = hmac.new(secret, time_bytes, hashlib.sha1).digest()
 
+# Get the last 4 bits of the hash
+offset = hash[-1] & 0x0F
+
+# Extract a 4-byte slice from the hash starting at the offset
+otp_bytes = hash[offset:offset+4]
+
+# Convert the bytes to an integer
+otp_int = struct.unpack('>I', otp_bytes)[0]
+
+# Generate the OTP as a 6 digit number
+otp_raw= otp_int % 10**6
+otp = '{:06d}'.format(otp_raw)
+
+print('Your OTP is :',otp)
