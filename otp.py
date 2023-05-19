1e992fa328fb3dff11a516d1f7b36c67c1994f22
cosBett <cosmasbett9@gmail.com>
2023-05-19 04:09:43 +0300

    hashing time steps

--- otp.py
+++ otp.py
@@ -7,3 +7,13 @@ secret = b'MysecretKey'
 
 time_step=300
 current_time =int(time.time())
+
+# Get the number of time steps
+time_steps = int(current_time / time_step)
+
+# Pack the time steps as a big-endian byte string
+time_bytes = struct.pack('>Q', time_steps)
+
+# Calculate the HMAC-SHA1 hash of the time steps using the secret key
+hash = hmac.new(secret, time_bytes, hashlib.sha1).digest()
+
